7fc1c627bb59931140c2b758b9b81f7b24f94b6d
lchapmb <lchapmb@googlemail.com>
2021-06-10 12:08:37 +0100

    amends win message

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cardsChosenId = [];
     resultDisplay.textContent = cardsWon.length;
     if (cardsWon.length === cardArray.length / 2) {
-      resultDisplay.textContent = 'Congratulations! You found them all!';
+      resultDisplay.textContent = 'Congratulations! You won!';
     }
   }
 
